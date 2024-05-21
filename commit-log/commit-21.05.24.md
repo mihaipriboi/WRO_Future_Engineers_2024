4ffeeaff44c948468960f0d5b3ecc4cfdc8823b0
PalamidaUsturoi <99142809+PalamidaUsturoi@users.noreply.github.com>
2024-05-21 15:08:55 +0300

    Commit - 21/05/2024 | Added electrical diagram and updated Arduino functions

---
+++ commit-log/commit-21.05.24.md
@@ -0,0 +1,3 @@
+# Commit - 27/01/2024 #3
+
+## Added electrical diagram and updated some functions for the Arduino part
---
+++ other/old-src/2_gyro.ino
@@ -0,0 +1,106 @@
+// Gyro sensor
+#define GYRO_SAMPLE_SIZE 1
+#define DRIFT_TEST_TIME 10
+#define INT_PIN A6
+
+// Gyro sensor
+Bmi088Gyro gyro(Wire,0x69);
+Bmi088Accel accel(Wire,0x19);
+
+// Gyro sensor
+double gyro_last_read_time = 0;
+double drifts_x, drifts_y, drifts_z;
+
+// Gyro functions
+void gyro_drdy() {
+  gyro_flag = true;
+}
+
+void accel_drdy() {
+  accel_flag = true;
+}
+
+void gyro_setup(bool debug) {
+  int status = accel.begin();
+  status = accel.setOdr(Bmi088Accel::ODR_200HZ_BW_80HZ);
+  status = accel.pinModeInt1(Bmi088Accel::PUSH_PULL,Bmi088Accel::ACTIVE_HIGH);
+  status = accel.mapDrdyInt1(true);
+
+
+  status = gyro.begin();
+
+  status = gyro.setOdr(Bmi088Gyro::ODR_400HZ_BW_47HZ);
+  status = gyro.pinModeInt3(Bmi088Gyro::PUSH_PULL,Bmi088Gyro::ACTIVE_HIGH);
+  status = gyro.mapDrdyInt3(true);
+
+  pinMode(INT_PIN,INPUT);
+  attachInterrupt(INT_PIN,gyro_drdy,RISING);
+
+  if(status < 0) {
+    if(debug){
+      Serial.print("BMI Initialization Error!  error: ");
+      Serial.println(status);
+    }
+  }
+  else  {
+    // Gyro drift calculation
+    if(debug) Serial.println("Starting gyro drift calculation...");
+
+    gx = 0;
+    gy = 0;
+    gz = 0;
+
+    gyro_last_read_time = millis();
+
+    double start_time = millis();
+    while(millis() - start_time < DRIFT_TEST_TIME * 1000) {
+      gyro.readSensor();  
+      double read_time = millis();
+      gx += (gyro.getGyroX_rads() * (read_time - gyro_last_read_time) * 0.001);
+      gy += (gyro.getGyroY_rads() * (read_time - gyro_last_read_time) * 0.001);
+      gz += (gyro.getGyroZ_rads() * (read_time - gyro_last_read_time) * 0.001);
+
+      gyro_last_read_time = read_time;
+    }
+
+    drifts_x = gx / DRIFT_TEST_TIME;
+    drifts_y = gy / DRIFT_TEST_TIME;
+    drifts_z = gz / DRIFT_TEST_TIME;
+
+    if(debug) Serial.print("Drift test done!\nx: ");
+    if(debug) Serial.print(drifts_x, 6);
+    if(debug) Serial.print("   y: ");
+    if(debug) Serial.print(drifts_y, 6);
+    if(debug) Serial.print("   z: ");
+    if(debug) Serial.println(drifts_z, 6);
+  }
+  // Gyro value reset
+  gx = 0;
+  gy = 0;
+  gz = 0;
+
+  gyro_last_read_time = millis();
+}
+
+void read_gyro(bool debug) {
+  if(gyro_flag) {
+    gyro_flag = false;
+    gyro.readSensor();   
+    double read_time = millis();
+
+    gx += ((gyro.getGyroX_rads() - drifts_x) * (read_time - gyro_last_read_time) * 0.001) * 180.0 / PI;
+    gy += ((gyro.getGyroY_rads() - drifts_y) * (read_time - gyro_last_read_time) * 0.001) * 180.0 / PI;
+    gz += ((gyro.getGyroZ_rads() - drifts_z) * (read_time - gyro_last_read_time) * 0.001) * 180.0 / PI;
+
+    gyro_last_read_time = read_time;
+
+    if(debug) {
+      Serial.print("Gyro: gx: ");
+      Serial.print(gx);
+      Serial.print(" gy: ");
+      Serial.print(gy);
+      Serial.print(" gz: ");
+      Serial.println(gz);
+    }
+  }
+}
